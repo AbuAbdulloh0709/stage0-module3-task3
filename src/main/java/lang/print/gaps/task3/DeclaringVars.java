5e4f38fe8d2f2452536668d9a815ac5f9fd23fca
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-10 15:29:50 +0500

    module3 task3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,9 +3,11 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
 
-        int a,b,c = 10;
+        int a, b, c;
 
-        System.out.println("c");
+        a = b = c = 10;
+
+        System.out.println(c);
 
     }
 }
